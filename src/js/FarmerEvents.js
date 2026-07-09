494e4a74df1ece28e3b9d169f7a17b862f208023
Qsx0391 <quansxing@gmail.com>
2026-07-09 21:49:19 +0800

    fix: 添加修复 from 3naka

--- src/js/FarmerEvents.js
+++ src/js/FarmerEvents.js
@@ -8,7 +8,7 @@ function farmerTendingDay() {
     let plantableList = getPlantableList();
     if (V.farm.farmer) {
         let updated = true;
-        while (workTime >= 60 && updated) {
+        while (workTime >= 10 && updated) {
             updated = false;
             // water
             if (V.farmerCanWater) {
@@ -22,22 +22,26 @@ function farmerTendingDay() {
                     }
                 }
             }
-            // till
+            // till + cultivator
             if (V.farmerCanTill) {
-                for (let i = 0; i < plotsFarm.length && workTime >= tillingTime; i++) {
+                for (let i = 0; i < plotsFarm.length; i++) {
                     let plot = plotsFarm[i];
+                    let tillingTimeTemp = V.farm.cultivator && (!plot.greenhouse || V.farm.cultivator >=2) ? 15 :tillingTime;
+                    if (tillingTimeTemp > workTime) break;
                     if (plot.stage === 0 && plot.till === 0) {
                         plot.till = 1;
-                        workTime -= tillingTime;
+                        workTime -= tillingTimeTemp;
                         updated = true;
                         console.log("till: " + i);
                     }
                 }
             }
-            // plant
+            // plant + seeder
             if (V.farmerCanPlant) {
-                for (let i = 0; i < plotsFarm.length && workTime >= 60; i++) {
+                for (let i = 0; i < plotsFarm.length; i++) {
                     let plot = plotsFarm[i];
+                    let planTime = V.farm.seeder && (!plot.greenhouse || V.farm.seeder >=2) ? 20 :180;
+                    if (planTime > workTime) break;
                     if (plot.stage === 0 && plot.till >= 1) {
                         let plantType, n;
                         if (plot.greenhouse && (n = V.farmerPlantable.length) > 0) {
@@ -48,27 +52,32 @@ function farmerTendingDay() {
                         if (plantType) {
                             plantSeedsInPlot(plot, plantType);
                             plot.water = V.farm.irrigation > i ? 1 : 0;
-                            workTime -= 180;
+                            workTime -= planTime;
                             updated = true;
                             console.log("plant: " + i + " " + plantType);
                         }
                     }
                 }
             }
-            // harvest
+            // harvest + harvester
             if (V.farmerCanHarvest) {
-                for (let i = 0; i < plotsFarm.length && workTime >= 60; i++) {
+                for (let i = 0; i < plotsFarm.length; i++) {
                     let plot = plotsFarm[i];
+                    let harvestTime = V.farm.harvester && (!plot.greenhouse || V.farm.harvester >=2) ? 10 :60;
+                    if (harvestTime > workTime) break;
                     if (plot.stage >= 5) {
                         let plantType = plot.plant;
-                        if (V.farm.stock[plantType] === undefined) {
+                        if (!V.farm.stock[plantType]) {
                             V.farm.stock[plantType] = 0;
                         }
-                        let tending_amount = random(10, Math.trunc(V.farm.farmer_skill / 2)) + 50;
+                        let tending_amount = random(10, Math.trunc(V.farm.farmer_skill / 2)+ 50) ;
+                        if(V.farm.harvester >=3)
+                            tending_amount = Math.trunc(V.farm.farmer_skill / 2) + random(10,50) ;
                         tending_amount *= setup.foodstuff[plantType].tending.yield_multiplier;
                         tending_amount *= 1 + (Math.clamp(plot.quality, 1, 4) - 1) * 0.2;
-                        tending_amount = Math.trunc(tending_amount * V.settings.tendingYieldModifier); //V.V.是何意味？有我什么不知道的特性吗？
-                        V.farm.stock[plantType] += tending_amount;
+                        tending_amount = Math.trunc(tending_amount * V.settings.tendingYieldModifier);
+                        if(tending_amount)
+                            V.farm.stock[plantType] += tending_amount;
                         if (plot.baseQuality && !V.backgroundTraits.includes("greenthumb")) {
                             if (plot.fertiliserDecay > 0) {
                                 plot.fertiliserDecay--;
@@ -85,7 +94,7 @@ function farmerTendingDay() {
                         plot.plant = "none";
                         plot.till = 0;
                         plot.stage = 0;
-                        workTime -= 60;
+                        workTime -= harvestTime;
                         updated = true; 
                         console.log("harvest: " + i + " " + plantType + " " + tending_amount);
                     }
@@ -114,19 +123,24 @@ function farmerSell() {
     }
 
     for (let item in V.farm.stock) {
-        if ((["truffles", "eggs", "milk"].includes(item) || ["produce", "vegetable", "fruit", "shroom"].includes(setup.foodstuff[item].category)) && V.farm.stock[item] >= 250) {
+        if ((["truffles", "eggs", "milk"].includes(item) || ["produce", "vegetable", "fruit", "mushroom"].includes(setup.foodstuff[item].category)) && V.farm.stock[item] >= 250) {
             const amount = Math.floor(V.farm.stock[item] / 250) * 250;
             V.farm.stock[item] -= amount;
             console.log("sell: " + item + " " + amount);
-            if (V.farmersProduce.selling[item] === undefined) {
-                V.farmersProduce.selling[item] = amount;
+             let Tkey = item;
+                if(item === "eggs") Tkey = "chicken_egg";
+                else if (item === "milk") Tkey = "bottle_of_milk";
+                else if (item === "truffles") Tkey = "truffle";
+            if (V.farmersProduce.selling[Tkey] === undefined) {
+                V.farmersProduce.selling[Tkey] = amount;
             } else {
-                V.farmersProduce.selling[item] += amount;
+                V.farmersProduce.selling[Tkey] += amount;
             }
         }
     }
 };
 window.farmerSell = farmerSell;
+
 
 function calcTillingTime() {
     let baseTime = 360;
@@ -138,7 +152,7 @@ function getPlantableList() {
     let result = [];
     V.farmerPlantable.forEach(each => {
         let plant = setup.foodstuff[each];
-        if (plant.tending.seasons.includes(Time.season)) {
+        if (plant.tending?.seasons?.includes(Time.season)) {
             result.push(each);
         }
     })
